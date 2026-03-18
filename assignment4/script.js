536bb1feda68e4bf3dc15db5c69d8bbbf46ba489
Harkirat <sainiharkirat38@gmail.com>
2026-03-18 21:29:51 +0530

    understand the math function and object

---
+++ assignment4/script.js
@@ -0,0 +1,21 @@
+// que 1 roll a dice and get random number
+let number =Math.floor( Math.random()*6 )+1;
+console.log(number);
+
+// que 2 object that represent car model
+const car={
+    name:"ferrai",
+    color: "red",
+    model:2012
+};
+console.log(car.name)
+
+// que3
+const detail={
+    name:"Ansh",
+    city:"chicago",
+    age:18
+}
+detail.city="New york";
+detail.country="United states";
+console.log(detail);
