3d81417956f3245be09c6885fe1890fb6c2d8c47
Harkirat <sainiharkirat38@gmail.com>
2026-03-21 10:30:25 +0530

    learned the concept of try and catch method

--- assignment6/script.js
+++ assignment6/script.js
@@ -1,3 +1,17 @@
+// try and catch method  - this method is basically use to catch the easily 
+let num=5;
+try{
+    console.log(num);
+}
+catch(err){
+    console.log("error ");
+    console.log(err)
+}
+console.log("hello");
+console.log("Riet");
+console.log(num);
+console.log("Jai hind");   
+
 const number =(n)=>{
     if(n%2==0){
         return "even";
