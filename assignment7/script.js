33e089c8a32de6c1e81474bd4706ac3115a145f9
Harkirat <sainiharkirat38@gmail.com>
2026-03-21 14:19:38 +0530

    learned about every some and reduce method

--- assignment7/script.js
+++ assignment7/script.js
@@ -14,3 +14,27 @@ let result =numbers.filter((el)=>{
 }
 );
 console.log(result);
+
+// every and some
+let a=[1,2,3,4];
+let even=a.some((el)=>{
+   return el%2==0;
+});
+console.log(even);
+let b=a.every((el)=>{
+   return el%2==0;
+});
+console.log(b);
+
+// que to understand reduce method(printing the lowest number in a array)
+let minimum=[5,4,2,7,10,9,25,29];
+let minimumnum=minimum.reduce((min,el)=>{
+    if(min<el){
+        return min
+    }
+    else{
+        return el;
+    }
+});
+console.log(minimumnum);
+
