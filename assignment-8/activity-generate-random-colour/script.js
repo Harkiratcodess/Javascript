25c64710355d3983e2d47ca7834432c4bd00652a
Harkirat <sainiharkirat38@gmail.com>
2026-03-22 11:23:01 +0530

    learned about dom and event listener and make a random colour generator

---
+++ assignment-8/activity-generate-random-colour/script.js
@@ -0,0 +1,19 @@
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    console.log("colour generated");
+    let randomcolour=changecolor();
+    let h1=document.querySelector("h1");
+    h1.innerText=randomcolour;
+    console.log("clour updated");
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomcolour;
+    
+});
+function changecolor(){
+    let red=Math.floor(Math.random() *255);
+    let green=Math.floor(Math.random() *255);
+    let blue=Math.floor(Math.random () *255);
+    let colour = `rgb(${red}, ${green}, ${blue})`;
+    return colour;
+}
