3293db48abfa6e1005175dc4794f54757a790df8
Harkirat <sainiharkirat38@gmail.com>
2026-03-22 14:49:35 +0530

    made a todo list using html js and css

---
+++ mini-project/script.js
@@ -0,0 +1,24 @@
+let input=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+    let li=document.createElement("li");
+    li.innerText=input.value;
+
+    let delbtn=document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+    li.appendChild(delbtn);
+    ul.appendChild(li);
+    input.value="";
+});
+
+ul.addEventListener("click",function(event){
+        if(event.target.nodeName=="BUTTON"){
+        let listitems=event.target.parentElement;
+        listitems.remove();
+        console.log("Deleted succesfully");
+        }
+});
+
